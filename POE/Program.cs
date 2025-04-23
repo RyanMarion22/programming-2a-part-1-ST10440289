df20f3a805d8e37d9b8ab2d5c4c457be6500c1aa
Scamp_Brutus5 <ST10440289@vcconnect.edu.za>
2025-04-23 16:31:17 +0200

    removed comment

--- POE/Program.cs
+++ POE/Program.cs
@@ -66,7 +66,7 @@ namespace CybersecurityAwarenessBot
             }
         }
 
-        // Display the lock ASCII art on startup
+      
         static void DisplayAsciiArt()
         {
             string asciiArt = @"
